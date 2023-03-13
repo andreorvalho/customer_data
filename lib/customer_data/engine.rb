1ad24e03cc885cd24bf1b4bc11bad5228d55d361
andre.orvalho <andre.orvalho@gmail.com>
2023-03-13 15:00:31 +0000

    added migrations path for exportation

--- lib/customer_data/engine.rb
+++ lib/customer_data/engine.rb
@@ -5,5 +5,7 @@ module CustomerData
     config.generators do |g|
       g.test_framework :rspec
     end
+
+    config.paths["db/migrate"].unshift(self.root.to_s + "/db/customer_data_migrate")
   end
 end
