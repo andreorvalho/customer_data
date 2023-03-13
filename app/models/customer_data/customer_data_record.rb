efb8f0b85c823a4714a60d3f17688c9116ccd765
andre.orvalho <andre.orvalho@gmail.com>
2023-03-13 13:40:34 +0000

    fix naming issues

--- app/models/customer_data/customer_data_record.rb
+++ app/models/customer_data/customer_data_record.rb
@@ -2,6 +2,6 @@ module CustomerData
   class CustomerDataRecord < ApplicationRecord
     self.abstract_class = true
 
-    connects_to database: { writing: :payment_gateway, reading: :payment_gateway  }
+    connects_to database: { writing: :customer_data, reading: :customer_data  }
   end
 end
